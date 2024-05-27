b42ce99f529f190cb6d9706de3a76bb2e84a5fa6
Alif Ahmed <alifahmed102@gmail.com>
2024-05-27 15:16:54 +0600

    'gdjds'

--- src/Pages/Sportspage/Sports.jsx
+++ src/Pages/Sportspage/Sports.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-// import im1 from '../../assets/Images/car5.jpg'
+import im1 from '../../assets/Images/car5.jpg'
 const Sports = () => {
   return (
-    <div className=' flex text-2xl justify-center text-center'>
+    <div className=' flex justify-center text-center'>
       <h1>Sports</h1>
-      {/* <img className='w-56' src={im1}></img> */}
+      <img className='w-56' src={im1}></img>
     </div>
   )
 }
