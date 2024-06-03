0f607bab6b59361c70ae1eb9efd1a9c462fe9e85
Alif Ahmed <alifahmed102@gmail.com>
2024-06-03 18:57:18 +0600

    'ogmeta'

--- src/Component/Sports/Sport.jsx
+++ src/Component/Sports/Sport.jsx
@@ -1,89 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import CategoryComponent from '../Binodon/CategoryComponent/CategoryComponent';
 
 const Sport = () => {
 
-    const [posts, setPosts] = useState([]);
-
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get('https://admin.desh365.top/api/all-post');
-            // console.log('Response data:', response.data.data); 
-           
-            const filteredPosts = response.data.data.filter(post => post.category_name === "স্পোর্টস");
-            setPosts(filteredPosts);
-          
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
     
-        fetchData();
-      }, []);
 
    
    
     return (
         <div>
-            <p className='font-bold text-xl m-6 ml-10'>স্পোর্টস</p>
-
-            <div className='grid md:grid-cols-12 md:mx-10 mx-2 gap-6 lg:grid-cols-12 grid-cols-1'>
-
-
-                <div className='col-span-4'>
-                    {posts.length > 0 && (
-            <div key={posts[0].id} className='space-y-4'>
-              <img className='w-[100%] h-full mb-3' src={`https://admin.desh365.top/public/storage/post-image/${posts[0].image}`} alt={posts[0].title} />
-              <h1 className='text-xl font-bold'>
-                {posts[0].title}
-              </h1>
-            </div>
-          )}
-                  
-                  
-                </div>
-
-
-                <div className='col-span-4'>
-                <div>
-    
-                <div className=' flex flex-col space-y-4 gap-3 py-4'>
-{posts.map(post => {
-        const imageUrl = `https://admin.desh365.top/public/storage/post-image/${post.image}`;
-        
-        return (
-         <Link to={`/details/${post?.id}`}  key={post?.id}>
-          <div className='flex gap-2' key={post?.id}>
-                        <img className='w-24 h-24' src={imageUrl} alt={post.title} />
-
-            <h2>{post.title}</h2>
-          </div>
-          </Link>
-        );
-      })}
-    </div>
-    </div>
-                </div>
-
-                {/* <div className='col-span-4'>
-                    <div className="flex items-center justify-center w-[90%] h-[350px]  font-bold bg-[#D9D9D9]">ADD</div>
-                </div> */}
-
-
-
-
-
-
-
-
-
-            </div>
-            <div className=' border-black w-[94%] mx-auto my-6 border-b-2'></div>
-
+          <CategoryComponent  categoryName="স্পোর্টস" />
         </div>
     )
 }
